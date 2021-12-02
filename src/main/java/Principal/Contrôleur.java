d17266ab24f2aa4c23836315b4b7c818921930cd
stanislasallouche <stanislasallouche@192.168.0.11>
2021-12-02 17:27:58 +0100

    Test

---
+++ src/main/java/Principal/Contrôleur.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Principal;
+
+/**
+ *
+ * @author stanislasallouche
+ */
+public class Contrôleur {
+    
+    private int état; 
+}
