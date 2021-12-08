e95bfa287c8fe13726cf41eb79c2cf836daf439b
stanislasallouche <stanislasallouche@192.168.0.11>
2021-12-08 19:30:50 +0100

    Connexion

--- src/main/java/Principal/VuePrincipale.java
+++ src/main/java/Principal/VuePrincipale.java
@@ -1,26 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Principal;
 
+import static Principal.Login.inscriptionExistsA;
+import static Principal.Login.inscriptionExistsE;
+import static Principal.Schema.connectPostgresql;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.login.LoginOverlay;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 
 /**
  *
@@ -32,11 +33,6 @@ public class VuePrincipale extends VerticalLayout {
 
     private Button Connection;
     private Button Élève;
-    private Button Administrateur;
-    private Button cancelA;
-    private Button cancelE;
-    private Button LoginE;
-    private Button LoginA;
     private Button Groupe1;
     private Button Groupe2;
     private Button Groupe3;
@@ -44,279 +40,82 @@ public class VuePrincipale extends VerticalLayout {
     private Button Logout;
     private Contrôleur Controleur;
     private HorizontalLayout HL;
-    private HorizontalLayout HLA;
-    private HorizontalLayout HLE;
-    
+    private Connection con;
+    private Login Log;
     private int etat;
+    private LoginOverlay LG;
 
-    public VuePrincipale(int etat) throws IOException {
-
-        Dialog dialog = new Dialog();
-        dialog.getElement().setAttribute("aria-label", "Create new employee");
-        Button Connection = new Button("Connection", e -> dialog.open());
-        VerticalLayout dialogLayout = Choix(dialog);
-        dialog.add(dialogLayout);
-        add(dialog, Connection);
+    
+    
+    public VuePrincipale(int etat) throws IOException, ClassNotFoundException, SQLException {
 
         
+
         this.Groupe1 = new Button("Groupe1");
         this.Groupe2 = new Button("Groupe2");
         this.Groupe3 = new Button("Groupe3");
         this.Groupe4 = new Button("Groupe4");
         this.Connection= new Button("Connexion");
-        this.LoginA= new Button("Login");
-        this.LoginE= new Button("Login");
-        this.cancelA= new Button("Cancel");
-        this.cancelE= new Button("Cancel");
-        this.Logout= new Button ("Déconnexion");
-        this.HLE= new HorizontalLayout();
-        this.HLA= new HorizontalLayout();
-        
-        HLA.add(Logout);
-        HLE.add(Logout);
-        
-          this.HL = new HorizontalLayout();
-        HL.add(Groupe1, Groupe2, Groupe3, Groupe4);
-        add(HL, HLE, HLA);
+        this.LG= new LoginOverlay();
         
         
+        this.Connection.addClickListener((t)->{
+            this.add(new Login(this, Connection, this.LG));
+        });
+
+        List<String> mdp = Administrateur.mdp();
+        List<String> email = Administrateur.email();
+        
+        this.LG.addLoginListener(event ->{
+            int j=0;
+            for (int i=0; i< email.size(); i++){
+                if ( event.getUsername()== email.get(i) && event.getPassword()== mdp.get(i)){
+                    Notification.show("aaaa");
+                    j=j+1;
+                }
+               if (j>0){
+                   Notification.show("Erreur dans la connexion!");
+               }
+               
+               
+            }
+           
+        });
+        
+        this.Logout = new Button("Déconnexion");
+
+        this.HL = new HorizontalLayout();
+
         this.Controleur = new Contrôleur(this);
         this.Controleur.changeEtat(etat);
+
+        this.con = connectPostgresql("localhost", 5432,
+                "postgres", "postgres", "passe");
+
+        HL.add(Groupe1, Groupe2, Groupe3, Groupe4);
+        add(HL, Connection);
+        
+        
+
         
         
         
     }
-    
-    public VuePrincipale() throws IOException{
+
+    public VuePrincipale() throws IOException, ClassNotFoundException, SQLException {
         this(1);
     }
-    
-    
-    
-    
+
+  
     
 
-    public VerticalLayout Choix(Dialog dialog) throws IOException {
-
-        H2 headline = new H2("Vous êtes: ");
-        headline.getStyle().set("margin", "var(--lumo-space-m) 0 0 0")
-                .set("font-size", "1.5em").set("font-weight", "bold");
-
-        this.Élève = new Button("Un Élève");
-        this.Administrateur = new Button("Un Administrateur");
-
-        Élève.addClickListener(event -> {
-            try {
-                dialog.removeAll();
-                Élève.addThemeVariants(ButtonVariant.LUMO_SMALL);
-                VerticalLayout M = ConnexionE(dialog);
-
-                dialog.add(M);
-                Élève.setVisible(false);
-                Administrateur.setVisible(false);
-
-            } catch (IOException ex) {
-                Logger.getLogger(VuePrincipale.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            
-            Élève.setVisible(true);
-            Administrateur.setVisible(true);
-        });
-
-        Administrateur.addClickListener(event -> {
-            try {
-                dialog.removeAll();
-                Élève.setVisible(false);
-                Administrateur.setVisible(false);
-                Administrateur.addThemeVariants(ButtonVariant.LUMO_SMALL);
-                VerticalLayout H = ConnexionA(dialog);
-                dialog.add(H);
-            } catch (IOException ex) {
-                Logger.getLogger(VuePrincipale.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-            Élève.setVisible(true);
-            Administrateur.setVisible(true);
-        });
-
-        HorizontalLayout buttonLayout = new HorizontalLayout(Élève,
-                Administrateur);
-
-        buttonLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
-
-        VerticalLayout dialogLayout = new VerticalLayout(headline,
-                buttonLayout);
-        dialogLayout.setPadding(false);
-        dialogLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
-        dialogLayout.getStyle().set("width", "300px").set("max-width", "100%");
-
-        return dialogLayout;
-    }
-
-    public VerticalLayout ConnexionA(Dialog dialog) throws IOException {
-
-        H2 headline = new H2("Connexion d'un Administrateur: ");
-        headline.getStyle().set("margin", "var(--lumo-space-m) 0 0 0")
-                .set("font-size", "1.5em").set("font-weight", "bold");
-
-        TextField firstNameField = new TextField("Login");
-        TextField lastNameField = new TextField("Password");
-        String Email = firstNameField.getValue();
-        String MDP= lastNameField.getValue();
-        VerticalLayout fieldLayout = new VerticalLayout(firstNameField,
-                lastNameField);
-        fieldLayout.setSpacing(false);
-        fieldLayout.setPadding(false);
-        fieldLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
-
-        
-
-        this.cancelA.addClickListener((event -> {
-            try {
-                dialog.removeAll();
-
-                VerticalLayout H = Choix(dialog);
-                dialog.add(H);
-            } catch (IOException ex) {
-                Logger.getLogger(VuePrincipale.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        })
-        );
-
-        
-        //FAIRE LE CLICKLISTENER POUR VÉRIFIER SI C'EST BON !
-        
-        this.LoginA.addClickListener((event -> {
-            Connection con = ConnectSGBD.connectionLocalPostgresql();
-            try {
-                if (inscriptionExistsA(con, Email, MDP)== false){
-                    fieldLayout.add("Erreur sur le login ou le MDP");
-                } else if (inscriptionExistsA(con, Email, MDP) ==true){
-                    this.Controleur.changeEtat(10);
-                }
-            } catch (SQLException ex) {
-                Logger.getLogger(VuePrincipale.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        
-        }));
-
-        this.LoginA.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        HorizontalLayout buttonLayout = new HorizontalLayout(this.cancelA,
-                this.LoginA);
-        buttonLayout
-                .setJustifyContentMode(FlexComponent.JustifyContentMode.END);
-
-        VerticalLayout dialogLayout = new VerticalLayout(headline, fieldLayout,
-                buttonLayout);
-        dialogLayout.setPadding(false);
-        dialogLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
-        dialogLayout.getStyle().set("width", "300px").set("max-width", "100%");
-
-        return dialogLayout;
-    }
-
-    public VerticalLayout ConnexionE(Dialog dialog) throws IOException {
-
-        H2 headline = new H2("Connexion d'un Élève");
-        headline.getStyle().set("margin", "var(--lumo-space-m) 0 0 0")
-                .set("font-size", "1.5em").set("font-weight", "bold");
-
-        TextField firstNameField = new TextField("Login");
-        TextField lastNameField = new TextField("Password");
-        String Email= firstNameField.getValue();
-        String MDP= lastNameField.getValue();
-        
-        VerticalLayout fieldLayout = new VerticalLayout(firstNameField,
-                lastNameField);
-        fieldLayout.setSpacing(false);
-        fieldLayout.setPadding(false);
-        fieldLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
-        
-        
-
-        
-
-        this.cancelE.addClickListener((event -> {
-            try {
-                dialog.removeAll();
-
-                VerticalLayout H = Choix(dialog);
-                dialog.add(H);
-            } catch (IOException ex) {
-                Logger.getLogger(VuePrincipale.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        })
-        );
-
-        
-        
-        this.LoginE.addClickListener((event -> {
-            Connection con = ConnectSGBD.connectionLocalPostgresql();
-            try {
-                if (inscriptionExistsE(con, Email, MDP)== false){
-                    fieldLayout.add("Erreur sur le login ou le MDP");
-                } else if (inscriptionExistsE(con, Email, MDP) == true) {
-                    this.Controleur.changeEtat(200);
-                }
-            } catch (SQLException ex) {
-                Logger.getLogger(VuePrincipale.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }));
-               
-               
-                
-                
-
-        //FAIRE LE CLICKLISTENER POUR VÉRIFIER SI C'EST BON !
-        this.LoginE.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        HorizontalLayout buttonLayout = new HorizontalLayout(this.cancelE,
-                this.LoginE);
-        buttonLayout
-                .setJustifyContentMode(FlexComponent.JustifyContentMode.END);
-
-        VerticalLayout dialogLayout = new VerticalLayout(headline, fieldLayout,
-                buttonLayout);
-        dialogLayout.setPadding(false);
-        dialogLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
-        dialogLayout.getStyle().set("width", "300px").set("max-width", "100%");
-
-        return dialogLayout;
-    }
-
-    public static boolean inscriptionExistsE(Connection con, String email , String mdp) throws SQLException {
-        try ( Statement st = con.createStatement();  ResultSet test = st.executeQuery("select * from etudiants where "
-                + "email = " + email
-                + " and mdp = " + mdp)) {
-            return test.next();
-        }
-    }
     
-    public static boolean inscriptionExistsA(Connection con, String email , String mdp) throws SQLException {
-        try ( Statement st = con.createStatement();  ResultSet test = st.executeQuery("select * from administrateur where "
-                + "email = " + email
-                + " and mdp = " + mdp)) {
-            return test.next();
-        }
-    }
-
-    
-   
-    
-    
-    
-    
-    
-    public Button getConnection() {
-        return Connection;
-    }
 
     public Button getÉlève() {
         return Élève;
     }
 
-    public Button getAdministrateur() {
-        return Administrateur;
-    }
+    
 
     public Button getGroupe1() {
         return Groupe1;
@@ -339,18 +138,8 @@ public class VuePrincipale extends VerticalLayout {
     }
 
     public HorizontalLayout getHL() {
+
         return HL;
     }
 
-    public HorizontalLayout getHLA() {
-        return HLA;
-    }
-
-    public HorizontalLayout getHLE() {
-        return HLE;
-    }
-
-    
-    
-    
 }
