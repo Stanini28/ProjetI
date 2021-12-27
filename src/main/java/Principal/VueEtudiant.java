c6835f03a6057007bba7314dcedcbc7c5b6004b8
stanislasallouche <stanislasallouche@192.168.0.11>
2021-12-27 11:37:34 +0100

    Avancé

--- src/main/java/Principal/VueEtudiant.java
+++ src/main/java/Principal/VueEtudiant.java
@@ -51,7 +51,7 @@ public class VueEtudiant extends Div {
 
     public VueEtudiant() throws ClassNotFoundException, SQLException {
         this.con = connectPostgresql("localhost", 5432,
-                "postgres", "postgres", "pass");
+                "postgres", "postgres", "passe");
         this.VL1 = new VerticalLayout();
         this.Module = new Tab("Description des Modules");
         this.Choix = new Tab("Voeux pour les Modules");
@@ -173,99 +173,110 @@ public class VueEtudiant extends Div {
         return Vl;
     }
 
-    public CheckboxGroup test() {
-        CheckboxGroup<String> checkboxGroup = new CheckboxGroup<>();
-        checkboxGroup.setLabel("Quel est votre Semestre?");
-        checkboxGroup.setItems("S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10");
-        checkboxGroup.addThemeVariants(CheckboxGroupVariant.LUMO_VERTICAL);
-        checkboxGroup.setSizeFull();
+    public HorizontalLayout test() throws SQLException {
+        HorizontalLayout HL = new HorizontalLayout();
+       Statement st = con.createStatement();
+       ResultSet res = st.executeQuery("select * from semestre");
+       while(res.next()){
+          String Num = res.getString("numerosem");
+          
+          Button M = new Button("S"+Num);
+          HL.add(M);
+          
+          M.addClickListener(event -> {
+             HL.setVisible(false);
+             this.Sem.setVisible(false);
+             this.Semestre=Integer.parseInt(Num);
+             this.Choix.setVisible(true);
+             this.Module.setVisible(true);
+             this.Historique.setVisible(true);
+          });
+       }
+       
+//        checkboxGroup.addSelectionListener(event -> {
+//            if (checkboxGroup.isSelected("S1")) {
+//                checkboxGroup.setVisible(false);
+//                this.Sem.setVisible(false);
+//                this.Semestre = 1;
+//                
+//            }
+//            if (checkboxGroup.isSelected("S2") == true) {
+//                checkboxGroup.setVisible(false);
+//                this.Semestre = 2;
+//                this.Sem.setVisible(false);
+//                this.Choix.setVisible(true);
+//                this.Module.setVisible(true);
+//                this.Historique.setVisible(true);
+//            }
+//            if (checkboxGroup.isSelected("S3") == true) {
+//                checkboxGroup.setVisible(false);
+//                this.Semestre = 3;
+//                this.Sem.setVisible(false);
+//                this.Choix.setVisible(true);
+//                this.Module.setVisible(true);
+//                this.Historique.setVisible(true);
+//                
+//            }
+//            if (checkboxGroup.isSelected("S4") == true) {
+//                checkboxGroup.setVisible(false);
+//                this.Semestre = 4;
+//                this.Sem.setVisible(false);
+//                this.Choix.setVisible(true);
+//                this.Module.setVisible(true);
+//                this.Historique.setVisible(true);
+//            }
+//            if (checkboxGroup.isSelected("S5") == true) {
+//                checkboxGroup.setVisible(false);
+//                this.Semestre = 5;
+//                this.Sem.setVisible(false);
+//                this.Choix.setVisible(true);
+//                this.Module.setVisible(true);
+//                this.Historique.setVisible(true);
+//            }
+//            if (checkboxGroup.isSelected("S6") == true) {
+//                checkboxGroup.setVisible(false);
+//                this.Semestre = 6;
+//                this.Sem.setVisible(false);
+//                this.Choix.setVisible(true);
+//                this.Module.setVisible(true);
+//                this.Historique.setVisible(true);
+//            }
+//            if (checkboxGroup.isSelected("S7") == true) {
+//                checkboxGroup.setVisible(false);
+//                this.Semestre = 7;
+//                this.Sem.setVisible(false);
+//                this.Choix.setVisible(true);
+//                this.Module.setVisible(true);
+//                this.Historique.setVisible(true);
+//            }
+//            if (checkboxGroup.isSelected("S8") == true) {
+//                checkboxGroup.setVisible(false);
+//                this.Semestre = 8;
+//                this.Sem.setVisible(false);
+//                this.Choix.setVisible(true);
+//                this.Module.setVisible(true);
+//                this.Historique.setVisible(true);
+//            }
+//            if (checkboxGroup.isSelected("S9") == true) {
+//                checkboxGroup.setVisible(false);
+//                this.Semestre = 9;
+//                this.Sem.setVisible(false);
+//                this.Choix.setVisible(true);
+//                this.Module.setVisible(true);
+//                this.Historique.setVisible(true);
+//            }
+//            if (checkboxGroup.isSelected("S10") == true) {
+//                checkboxGroup.setVisible(false);
+//                this.Semestre = 10;
+//                this.Sem.setVisible(false);
+//                this.Choix.setVisible(true);
+//                this.Module.setVisible(true);
+//                this.Historique.setVisible(true);
+//            }
+//
+//        });
 
-        checkboxGroup.addSelectionListener(event -> {
-            if (checkboxGroup.isSelected("S1")) {
-                checkboxGroup.setVisible(false);
-                this.Sem.setVisible(false);
-                this.Semestre = 1;
-                this.Choix.setVisible(true);
-                this.Module.setVisible(true);
-                this.Historique.setVisible(true);
-            }
-            if (checkboxGroup.isSelected("S2") == true) {
-                checkboxGroup.setVisible(false);
-                this.Semestre = 2;
-                this.Sem.setVisible(false);
-                this.Choix.setVisible(true);
-                this.Module.setVisible(true);
-                this.Historique.setVisible(true);
-            }
-            if (checkboxGroup.isSelected("S3") == true) {
-                checkboxGroup.setVisible(false);
-                this.Semestre = 3;
-                this.Sem.setVisible(false);
-                this.Choix.setVisible(true);
-                this.Module.setVisible(true);
-                this.Historique.setVisible(true);
-                
-            }
-            if (checkboxGroup.isSelected("S4") == true) {
-                checkboxGroup.setVisible(false);
-                this.Semestre = 4;
-                this.Sem.setVisible(false);
-                this.Choix.setVisible(true);
-                this.Module.setVisible(true);
-                this.Historique.setVisible(true);
-            }
-            if (checkboxGroup.isSelected("S5") == true) {
-                checkboxGroup.setVisible(false);
-                this.Semestre = 5;
-                this.Sem.setVisible(false);
-                this.Choix.setVisible(true);
-                this.Module.setVisible(true);
-                this.Historique.setVisible(true);
-            }
-            if (checkboxGroup.isSelected("S6") == true) {
-                checkboxGroup.setVisible(false);
-                this.Semestre = 6;
-                this.Sem.setVisible(false);
-                this.Choix.setVisible(true);
-                this.Module.setVisible(true);
-                this.Historique.setVisible(true);
-            }
-            if (checkboxGroup.isSelected("S7") == true) {
-                checkboxGroup.setVisible(false);
-                this.Semestre = 7;
-                this.Sem.setVisible(false);
-                this.Choix.setVisible(true);
-                this.Module.setVisible(true);
-                this.Historique.setVisible(true);
-            }
-            if (checkboxGroup.isSelected("S8") == true) {
-                checkboxGroup.setVisible(false);
-                this.Semestre = 8;
-                this.Sem.setVisible(false);
-                this.Choix.setVisible(true);
-                this.Module.setVisible(true);
-                this.Historique.setVisible(true);
-            }
-            if (checkboxGroup.isSelected("S9") == true) {
-                checkboxGroup.setVisible(false);
-                this.Semestre = 9;
-                this.Sem.setVisible(false);
-                this.Choix.setVisible(true);
-                this.Module.setVisible(true);
-                this.Historique.setVisible(true);
-            }
-            if (checkboxGroup.isSelected("S10") == true) {
-                checkboxGroup.setVisible(false);
-                this.Semestre = 10;
-                this.Sem.setVisible(false);
-                this.Choix.setVisible(true);
-                this.Module.setVisible(true);
-                this.Historique.setVisible(true);
-            }
-
-        });
-
-        return checkboxGroup;
+        return HL;
     }
 
 //    public VerticalLayout AffichageGM(Dialog dialog) throws SQLException {
