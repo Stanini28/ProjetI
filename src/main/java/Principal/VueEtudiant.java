2f40fde65049e3e34e649760e56a7aa0f040cb34
stanislasallouche <stanislasallouche@192.168.0.11>
2021-12-28 10:07:15 +0100

    Fin voeux

--- src/main/java/Principal/VueEtudiant.java
+++ src/main/java/Principal/VueEtudiant.java
@@ -54,7 +54,7 @@ public class VueEtudiant extends Div {
 
     public VueEtudiant() throws ClassNotFoundException, SQLException {
         this.con = connectPostgresql("localhost", 5432,
-                "postgres", "postgres", "pass");
+                "postgres", "postgres", "passe");
         this.VL1 = new VerticalLayout();
         this.Module = new Tab("Description des Modules");
         this.Choix = new Tab("Voeux pour les Modules");
